4affc10596729818c7a8d06509a9e168ecfb539f
Ronak Ramchandra Dabade <Ronak@Ronaks-MBP.bwrouter>
2026-01-19 09:04:14 +0530

    Removed extra slash

--- src/Register.js
+++ src/Register.js
@@ -17,7 +17,7 @@ function Register({ onSuccess }) {
   };
 
   const submit = async () => {
-    const res = await fetch("https://backend-clean-lfp0.onrender.com//register", {
+    const res = await fetch("https://backend-clean-lfp0.onrender.com/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form)
